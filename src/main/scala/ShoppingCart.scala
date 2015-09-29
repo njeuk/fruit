bdbc797028d89d9d2254d270ff002787b7be054e
Nick Edwards <nick@comdevelopment.com>
2015-09-29 23:16:44 +0100

    step2

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -14,7 +14,26 @@ object ShoppingCart {
     case Orange => 25
   }
 
-  def checkOut(basket: List[Item]) = basket.foldLeft(0)(_ + priceUp(_))
+  def checkOut(basket: List[Item]) : Int = {
+
+    def applyOffers : List[Item] = {
+      def inner(apples: Int, oranges: Int, items: List[Item]) : List[Item] =
+        items match {
+          case Nil => List()
+          case Apple::t =>
+            if (apples == 1) inner(0, oranges, t)
+            else Apple::inner(apples + 1, oranges, t)
+          case Orange::t =>
+            if (oranges == 2) inner(apples, 0, t)
+            else Orange::inner(apples, oranges + 1, t)
+      }
+
+      inner(0, 0, basket)
+    }
+
+    val pricingBasket = applyOffers
+    pricingBasket.foldLeft(0)(_ + priceUp(_))
+  }
 
   def scanItems(items: List[String]) : List[Item] = {
     items match {
@@ -26,8 +45,5 @@ object ShoppingCart {
       }
     }
   }
-
-
-
 
 }
--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -10,7 +10,7 @@ class ShoppingCartSpec extends FlatSpec with Matchers {
   }
   
   "checkout" should "takes a list of items scanned and outputs the total cost" in {
-    ShoppingCart.checkOut(List(Apple, Apple, Orange, Apple)) should === (205)
+    ShoppingCart.checkOut(List(Apple, Apple, Orange, Apple)) should === (145)
   }
   "checkout" should "have a zero price for an empty list" in {
     ShoppingCart.checkOut(Nil) should === (0)
@@ -26,6 +26,23 @@ class ShoppingCartSpec extends FlatSpec with Matchers {
   }
 
   "shoppingCart process" should "take a list of Apples and Oranges, returning a price" in {
-    ShoppingCart.process(Array("Apple", "Apple", "Orange", "Apple")) should === ("£2.05")
+    ShoppingCart.process(Array("Apple", "Apple", "Orange", "Apple")) should === ("£1.45")
+    ShoppingCart.process(Array("Apple", "Apple", "Orange", "Apple", "Orange", "Orange")) should === ("£1.70")
+  }
+
+  "checkout" should "offer buy one, get one free on Apples" in {
+    ShoppingCart.checkOut(List(Apple)) should === (60)
+    ShoppingCart.checkOut(List(Apple, Apple)) should === (60)
+    ShoppingCart.checkOut(List(Apple, Apple, Apple)) should === (60 * 2)
+    ShoppingCart.checkOut(List(Apple, Apple, Apple, Apple)) should === (60 * 2)
+  }
+
+  "checkout" should "offer 3 for the price of 2 on Oranges" in {
+    ShoppingCart.checkOut(List(Orange)) should === (25)
+    ShoppingCart.checkOut(List(Orange, Orange)) should === (25 * 2)
+    ShoppingCart.checkOut(List(Orange, Orange, Orange)) should === (25 * 2)
+    ShoppingCart.checkOut(List.fill(4)(Orange)) should === (25 * 3)
+    ShoppingCart.checkOut(List.fill(5)(Orange)) should === (25 * 4)
+    ShoppingCart.checkOut(List.fill(6)(Orange)) should === (25 * 4)
   }
 }
